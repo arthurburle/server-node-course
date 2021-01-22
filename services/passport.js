0f98fefe5f2afd95ba3eb568eee922d58fd3b308
ArthurBurle <arthurburle@gmail.com>
2021-01-21 23:12:03 -0300

    acho q agora deu certo o mongodb

--- services/passport.js
+++ services/passport.js
@@ -20,8 +20,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL:
-        'https://shrouded-eyrie-24338.herokuapp.com/auth/google/callback',
+      callbackURL: '/auth/google/callback',
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
