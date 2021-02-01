37f3157bbc626de66327a0252cbfecd917b9177b
ArthurBurle <arthurburle@gmail.com>
2021-02-01 19:02:49 -0300

    starting billing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-  return <div>Hi there</div>;
-};
+import Header from './Header';
+import DashBoard from './DashBoard';
+import SurveyNew from './SurveyNew';
+import Landing from './Landing';
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" component={Landing} exact />
+            <Route path="/surveys" component={DashBoard} exact />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
